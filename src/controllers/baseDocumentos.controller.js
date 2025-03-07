c1b5871083251fcea1d8d1b3be6f4f44ff13a462
unknown <jonatanpacora@gmail.com>
2025-03-07 14:12:57 -0500

    Controlador Lista Detalle - Mod Base Documentos

--- src/controllers/baseDocumentos.controller.js
+++ src/controllers/baseDocumentos.controller.js
@@ -105,3 +105,38 @@ export const buscarPorCodigoIDC = async (req, res) => {
       }
     }
   };
+
+// Para Lista de Detalle Documento por ID
+export const getListaDetalleDocumentoPorID= async (req, res) => {
+  let pool;
+  const {idCarta, tipo}= req.body;
+  try {
+    const sql = `EXEC app.usp_ListaDetalleDocumentoXID ${idCarta} , '${tipo}'`;
+    pool = await getConnection();
+    const result = await pool.request().query(sql);
+
+    const detalle = result.recordsets.length; // ✅ Corrección de "length"
+
+    console.log("Resultado:", result.recordsets);
+
+    if (detalle> 0) {
+      pool.close();
+      return res.status(200).json({ status: 200, data: result.recordsets[0] });
+    } else {
+      pool.close();
+      return res.status(404).json({
+        status: 400,
+        error: "No se encontraron la lista detalle",
+      });
+    }
+  } catch (error) {
+    console.error("Error en listar el detalle :", error);
+    return res
+      .status(500)
+      .json({ error: "Error interno del servidor: " + error.message });
+  } finally {
+    if (pool) {
+      pool.close();
+    }
+  }
+};
--- src/routes/baseDocumentos.routes.js
+++ src/routes/baseDocumentos.routes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get('/obtenerListaDocumentosPendientes',baseDocumentosCtrl.getListaDocumentosPendientes);
 router.get('/obtenerListaDocumentosGenerados',baseDocumentosCtrl.getListaDocumentosGenerados);
+router.get('/obtenerListaDetalle',baseDocumentosCtrl.getListaDetalleDocumentoPorID);
 router.post('/listaDocumentosPorCodigoIDC',baseDocumentosCtrl.buscarPorCodigoIDC);
 
 
